e352b7db8b779e58135e1b534f7daac4e22152e8
Chris <chris.sewell@buildcircle.co.uk>
2023-10-11 18:13:13 +0100

    add bun test

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import getCharacters from "./getCharacters";
+
+function startBunServer() {
+  Bun.serve({
+    port: 8080,
+    fetch(req) {
+      const url = new URL(req.url);
+      if (url.pathname === "/battle") {
+        const characters = getCharacters()
+
+        const hero = characters.items.find(e => e.name === heroName)
+        const villain = characters.items.find(e => e.name === villainName)
+
+        const winner = hero.score >= villain.score ? hero : villain
+
+        return new Response(winner);
+      }
+      return new Response("404");
+    },
+  });
+}
+
+export default startBunServer;
---
+++ src/index.spec.js
@@ -0,0 +1,18 @@
+import { expect, test } from "bun:test";
+import startBunServer from "./index";
+import request from "supertest";
+const baseURL = "http://localhost:8080"
+
+startBunServer();
+
+test("Server returns 200", async () => {
+    const response = await request(baseURL).get("/battle?hero=Batman&villain=Joker");
+
+    expect(response.statusCode).toBe(200);
+});
+
+test('battle should return the hero if they have a higher score', async () => {
+    const response = await request(baseURL).get("/battle?hero=Batman&villain=Joker");
+
+    expect(response.body).toEqual({ name: 'Batman', score: 10 })
+})
